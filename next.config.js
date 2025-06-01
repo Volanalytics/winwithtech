b37f8633bc4158f12a3b5532b60b3aa2a4d96347
CodeSandbox <support@codesandbox.io>
2025-06-01 05:06:02 +0000

    fix: add basePath for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // Enable static exports
+  basePath: '/winwithtech', // The name of your GitHub repository
   images: {
     unoptimized: true,
     remotePatterns: [
