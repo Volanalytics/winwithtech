6fcd1c26eb2acca7a4026952fb8a84d57d12c5ff
CodeSandbox <support@codesandbox.io>
2025-06-01 14:38:56 +0000

    fix: add custom image loader and asset prefix

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ const nextConfig = {
   output: 'export',  // Enable static exports
   basePath: '/winwithtech', // The name of your GitHub repository
   trailingSlash: true, // Add trailing slashes to all URLs
+  assetPrefix: '/winwithtech/', // Prefix for all assets
   images: {
     unoptimized: true,
+    loader: 'custom',
+    loaderFile: './image-loader.js',
   },
 }
 
