7d63f3783cb9d2342c4671c1ee55e493fb271104
CodeSandbox <support@codesandbox.io>
2025-06-01 14:39:45 +0000

    fix: simplify Next.js configuration

--- next.config.js
+++ next.config.js
@@ -3,11 +3,8 @@ const nextConfig = {
   output: 'export',  // Enable static exports
   basePath: '/winwithtech', // The name of your GitHub repository
   trailingSlash: true, // Add trailing slashes to all URLs
-  assetPrefix: '/winwithtech/', // Prefix for all assets
   images: {
     unoptimized: true,
-    loader: 'custom',
-    loaderFile: './image-loader.js',
   },
 }
 
