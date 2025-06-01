7db4312c941ea2e135c6da3ab94c4592e43a36ec
CodeSandbox <support@codesandbox.io>
2025-06-01 04:35:14 +0000

    chore: add GitHub Pages configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'export',  // Enable static exports
+  basePath: '/winwithtech', // The name of your GitHub repository
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
